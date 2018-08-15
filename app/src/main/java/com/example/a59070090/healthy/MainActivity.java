2a419c7d8cbaf44357fae8a08e82fb8cba77ee43
adobebeta <adobebeta1997@gmail.com>
2018-08-15 11:35:38 +0700

    eiei

---
+++ app/src/main/java/com/example/a59070090/healthy/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.a59070090.healthy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+//        setContentView(R.layout.fregment_login);
+        setContentView(R.layout.fregment_register);
+    }
+}
